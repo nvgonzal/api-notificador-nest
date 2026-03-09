7ccba2a4a5367e4ad64bfd4c0af91d633bd162ce
Nicolas Gonzalez <nicolas.gonzalez.tardon@gmail.com>
2026-03-08 21:40:39 -0300

    Agrega validador y funciones de service user

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsString, IsNotEmpty, IsDate } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsString()
   lastName: string;
+  @IsString()
+  @IsNotEmpty()
   email: string;
+  @IsDate()
   bday: Date;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UsersService {
-
+  private readonly users: User[] = [];
+  create(user: User) {
+    return this.users.push(user);
+  }
+  getAll(): User[] {
+    return this.users;
+  }
+  getOne(id: number): User {
+    //TODO: Conectar a db
+  }
+  update(id: number){
+    //TODO: implementar db
+  }
+  delete(id: number){
+    //TODO implenetar db
+  }
 }
